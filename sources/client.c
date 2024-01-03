e49246e50c3dbb192da5e9bd13bed16064ffa54b
Mateus Santos do Nascimento <mateussantos1528@hotmail.com>
2024-01-03 13:54:18 -0300

    almoss donis

--- sources/client.c
+++ sources/client.c
@@ -6,13 +6,37 @@
 /*   By: matesant <matesant@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/04 17:05:12 by matesant          #+#    #+#             */
-/*   Updated: 2024/01/03 13:20:53 by matesant         ###   ########.fr       */
+/*   Updated: 2024/01/03 13:53:57 by matesant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minitalk.h"
 
-void	send_signal(unsigned char message, int pid);
+int	g_exit = 0;
+
+void	handle(int signal)
+{
+	if (signal == SIGUSR1)
+		g_exit = 1;
+}
+
+void	send_signal(unsigned char message, int pid)
+{
+	int	bits;
+
+	bits = 0;
+	while (bits < 8)
+	{
+		g_exit = 0;
+		if (message >> bits & 1)
+			kill(pid, SIGUSR2);
+		else
+			kill(pid, SIGUSR1);
+		bits++;
+		while (!g_exit)
+			;
+	}
+}
 
 int	main(int argc, char **argv)
 {
@@ -23,25 +47,12 @@ int	main(int argc, char **argv)
 	{
 		pid = atoi(argv[1]);
 		message = argv[2];
+		signal(SIGUSR1, handle);
+		signal(SIGUSR2, handle);
 		while (*message)
 		{
 			send_signal(*message, pid);
 			message++;
 		}
-	}
-}
-
-void	send_signal(unsigned char message, int pid)
-{
-	int	bits;
-
-	bits = 0;
-	while (bits < 8)
-	{
-		if (message >> bits & 1)
-			kill(pid, SIGUSR1);
-		else
-			kill(pid, SIGUSR1);
-		bits++;
 	}
 }
